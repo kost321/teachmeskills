9fe9536c6c0078e807e66bf61ac4c20326e5ebd6
kost321 <kostyannn1996@gmail.com>
2022-02-22 13:30:45 +0300

    swiper

---
+++ lesson7/src/js/script.js
@@ -0,0 +1,40 @@
+
+
+new Swiper('.customers-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        // when window width is <= 320px
+        576: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        // when window width is <= 480px
+        
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+
+        
+      }
+  });
